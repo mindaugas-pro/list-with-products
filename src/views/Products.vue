04706bf13755c45c858671ca3a9462f70d16e340
Mindaugas <48929117+mindaugas-pro@users.noreply.github.com>
2022-02-08 14:20:02 +0200

    component restructured

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,75 +1,116 @@
 <template>
   <section class="hero is-fullheight">
-    <div class="hero-head">Navigation bar</div>
+    <div class="hero-head">
+      <NavBar />
+    </div>
     <div class="hero-body">
       <div class="container">
-
+        
         <div class="table container">
           <table class="table is-fullwidth">
-          <thead>
-            <tr>
-              <th><abbr title="Code">Code</abbr></th>
-              <th>Name</th>
-              <th><abbr title="Base Price">Base Price</abbr></th>
-              <th><abbr title="Total Price">Total Price</abbr></th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(product, index) in productsLocalStorage"
-              :key="index"
-              >
-                <td>
-                  <input v-if="product.isEditMode" type="text" class="input" v-model="product.code">
-                  <span v-else>{{ product.code }}</span>
-                </td>
+            <thead>
+              <tr>
+                <th><abbr title="Code">Code</abbr></th>
+                <th>Name</th>
+                <th><abbr title="Base Price">Base Price</abbr></th>
+                <th><abbr title="Total Price">Total Price</abbr></th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(product, index) in productsLocalStorage"
+                :key="index"
+                >
+                  <td>
+                    <input v-if="product.isEditMode" type="text" class="input" v-model="product.code">
+                    <span v-else>{{ product.code }}</span>
+                  </td>
 
-                <td>
-                  <input v-if="product.isEditMode" type="text" class="input" v-model="product.name">
-                  <span v-else>{{ product.name }}</span>
-                </td>
-                <td>
-                  <input v-if="product.isEditMode" type="text" class="input" v-model="product.basePrice">
-                  <span v-else>{{ product.basePrice }}</span>
-                </td>
-                <td>{{ product.totalPrice }}</td>
-                <td>
-                  <button v-if="product.isEditMode" class="button mr-1 my-1 is-small" v-on:click="save(product, index)">save</button>
-                  <button v-if="product.isEditMode" class="button mr-1 my-1 is-small" v-on:click="cancel(product, index)">cancel</button>
-                  <button v-if="product.isEditMode" class="button mr-1 my-1 is-small" v-on:click="remove(product, index)">delete</button>
-                  <button v-if="!product.isEditMode" class="button" v-on:click="edit(product, index)">edit</button>
-                </td>
-            </tr>
+                  <td>
+                    <input v-if="product.isEditMode" type="text" class="input" v-model="product.name">
+                    <span v-else>{{ product.name }}</span>
+                  </td>
+                  <td>
+                    <input v-if="product.isEditMode" type="text" class="input" v-model="product.basePrice">
+                    <span v-else>{{ product.basePrice }}</span>
+                  </td>
+                  <td>{{ product.totalPrice }}</td>
+                  <td>
+                    <button v-if="product.isEditMode" class="button mr-1 my-1 is-small" v-on:click="save(product, index)">save</button>
+                    <button v-if="product.isEditMode" class="button mr-1 my-1 is-small" v-on:click="cancel(product, index)">cancel</button>
+                    <button v-if="product.isEditMode" class="button mr-1 my-1 is-small" v-on:click="remove(product, index)">delete</button>
+                    <button v-if="!product.isEditMode" class="button" v-on:click="edit(product, index)">edit</button>
+                  </td>
+              </tr>
 
-            <tr>
-              <td>Subtotal:</td>
-              <td></td>
-              <td>{{ subtotalBasePrice }}</td>
-              <td>{{ subtotalTotalPrice }}</td>
-              <td></td>
-            </tr>
+              <tr>
+                <td>Subtotal:</td>
+                <td></td>
+                <td>{{ subtotalBasePrice }}</td>
+                <td>{{ subtotalTotalPrice }}</td>
+                <td></td>
+              </tr>
 
-            <tr v-if="isEmptyLine">
-              <td>{{ }}</td>
-              <td>{{ }}</td>
-              <td>{{ }}</td>
-              <td>{{ }}</td>
-              <td><button class="button is-success" v-on:click="addNewLine()">add</button></td>
-            </tr>
-            <tr v-else>
-              <td><input type="text" class="input" placeholder="code" v-model="code"></td>
-              <td><input type="text" class="input" placeholder="name" v-model="name"></td>
-              <td><input type="text" class="input" placeholder="base price" v-model="basePrice"></td>
-              <td></td>
-              <td><button class="button" v-on:click="addNew()">add</button></td>
-              <td><button class="button" v-on:click="addNew()">cancel</button></td>
-            </tr>
-          </tbody>
-        </table>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td v-if="!isNewLine"><button class="button is-success" v-on:click="addNewLine()">Add New</button></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-        <button class="button is-link" v-on:click="next()">Continue</button>
+        <form v-if="isNewLine" @submit="submit">
+          <div class="table container">
+            <table class="table is-fullwidth">
+              <tbody>
+                <tr>
+                  <td>
+                    <div class="field">
+                      <div class="control">
+                        <input type="text" class="input" placeholder="code" v-model="code" :error="codeError">
+                        <p class="help is-danger">{{ codeError }}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="field">
+                      <div class="control">
+                        <input type="text" class="input" placeholder="name" v-model="name" :error="nameError">
+                        <p class="help is-danger">{{ nameError }}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="field">
+                      <div class="control">
+                        <input type="text" class="input" placeholder="price" v-model="basePrice" :error="basePriceError">
+                        <p class="help is-danger">{{ basePriceError }}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="field is-grouped">
+                      <div class="control">
+                        <button class="button is-link">Submit</button>
+                      </div>
+                      <div class="control">
+                        <button class="button is-danger" @click='cancelNewLine()'>cancel</button>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </form>
+
+        <section class="section" v-if="!isNewLine">
+          <button class="button is-link" v-on:click="$router.push('/billPage')">Next</button>
+        </section>
 
       </div>
     </div>
@@ -80,15 +121,15 @@
 <script>
 import { computed, ref, watch } from 'vue'
 import { useStore } from 'vuex'
+import { useField, useForm, useResetForm } from 'vee-validate'
+import { number, string } from 'yup'
+import NavBar from '../components/NavBar.vue'
 
 export default {
+    components: { NavBar },
     setup() {
         let productsLocalStorage = ref([])
-        let isEmptyLine = ref(true)
         let isNewLine = ref(false)
-        let code = ref('')
-        let name = ref('')
-        let basePrice = ref('')
         let taxValue = ref(0.21)
         const store = useStore()
 
@@ -109,10 +150,69 @@ export default {
                 store.commit('SET_SUBTOTAL_TOTAL_PRICE', value)
             }
         })
+        
+        const validationss = {
+          code: number().required().min(3),
+          name: string().required().min(3),
+          basePrice: number().required()
+        }
+
+        // const validations = {
+        //   code: value => {
+        //     if (value && value.trim()) {
+        //       return true;
+        //     }
+
+        //       return 'This field is required';
+        //     },
+        //   name: value => {
+        //     if (value && value.trim()) {
+        //       return true;
+        //     }
+
+        //     return 'This field is required';
+        //   },
+        //   basePrice: value => {
+        //     if (value && value.trim()) {
+        //       return true;
+        //     }
+
+        //     return 'This field is required';
+        //   },
+        // }
+
+        const { handleSubmit } = useForm({
+          validationSchema: validationss
+        })
+
+        const submit = handleSubmit((values, { resetForm }) => {
+          basePrice.value = parseFloat(basePrice.value.replace(',', '.').replace(' ', '')) // convert commas to dots to avoid NaN
+          const totalPrice = countTax(basePrice.value)
+          const product = { code: code.value, name: name.value, basePrice: basePrice.value, totalPrice: totalPrice }
+          productsLocalStorage.value.push(product)
+          localStorage.setItem('products', JSON.stringify(productsLocalStorage.value))
+          isNewLine.value = false
+          code.value = ''
+          name.value = ''
+          basePrice.value = ''
+          resetForm() // https://vee-validate.logaretm.com/v4/guide/composition-api/handling-forms#handling-submissions
+        })
+
+        const { value: code, errorMessage: codeError } = useField('code')
+        const { value: name, errorMessage: nameError } = useField('name')
+        const { value: basePrice, errorMessage: basePriceError } = useField('basePrice')
+        
+        const resetForm = useResetForm()
+
+        function cancelNewLine () {
+          resetForm(); // resets the form
+          isNewLine.value = false
+        }
 
         function createLocalStorage () {
-            if (localStorage.getItem('products') === null || productsLocalStorage.value.length === 0) {
-                console.log('func createLocalStorage if triggered')
+          let tempLocalStorageArray = JSON.parse(localStorage.getItem('products'))
+            if (tempLocalStorageArray.length === 0) {
+                // console.log('func createLocalStorage if triggered')
                 const products = [
                 { code: '12345', name: 'T-shirt', basePrice: '14.78', totalPrice: null },
                 { code: '54321', name: 'Cardigan', basePrice: '34.56', totalPrice: null }
@@ -182,23 +282,16 @@ export default {
         }
 
         function addNewLine () {
-            isEmptyLine.value = false
+            isNewLine.value = true
         }
 
-        function addNew () {
-            if (!code.value && !name.value && !basePrice.value) {
-                isEmptyLine.value = true
-            } else {
-                basePrice.value = parseFloat(basePrice.value.replace(',', '.').replace(' ', '')) // convert commas to dots to avoid NaN
-                const totalPrice = countTax(basePrice.value)
-                const product = { code: code.value, name: name.value, basePrice: basePrice.value, totalPrice: totalPrice }
-                productsLocalStorage.value.push(product)
-                localStorage.setItem('products', JSON.stringify(productsLocalStorage.value))
-                isEmptyLine.value = true
-                code.value = ''
-                name.value = ''
-                basePrice.value = ''
-            }
+        function cancelAddNew () {
+          const { setValues } = useForm();
+          setValues({
+            code: '',
+            name: '',
+            basePrice: ''
+          })
         }
 
         createLocalStorage()
@@ -215,14 +308,16 @@ export default {
 
         return {
             productsLocalStorage,
-            isEmptyLine,
             isNewLine,
-            code,
             name,
+            nameError,
             basePrice,
+            basePriceError,
             taxValue,
             subtotalBasePrice,
             subtotalTotalPrice,
+            codeError,
+            code,
             createLocalStorage,
             setProductsLocalStorage,
             countTotalPrice,
@@ -233,7 +328,9 @@ export default {
             save,
             remove,
             addNewLine,
-            addNew
+            cancelAddNew,
+            submit,
+            cancelNewLine
         }
     },
 }

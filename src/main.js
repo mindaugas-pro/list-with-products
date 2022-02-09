e43bd1a9664705def17d7b80d4e8bb5040b9e06b
Mindaugas <48929117+mindaugas-pro@users.noreply.github.com>
2022-02-09 12:54:05 +0200

    new production version created

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '../node_modules/bulma/css/bulma.css' // <-- import Bulma CSS
-import router from './router' // <-- import router
-import store from './store' // <-- import store
-
-createApp(App).use(router).use(store).mount('#app') // <-- use router
